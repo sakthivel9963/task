39e654a437b3ea8bab5ff9ae197c9ea3bc4f3c0c
sakthivel9963 <sakthivel9963@gmail.com>
2021-08-04 20:16:25 +0530

    rate limiter for all the api request implemented

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const path = require('path');
+const rateLimit = require('express-rate-limit');
 const router = require('./router');
 const { notFound, errorHandler } = require('./middleware/defaults');
-require('dotenv').config();
 const winston = require('./middleware/winston');
 
 const app = express();
@@ -17,6 +18,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
 app.use(morgan('combined', { stream: winston.stream }));
+
+// ratelimiter
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 200, // limit each IP to 100 requests per windowMs
+  message:
+    'Too many accounts created from this IP, please try again after an 15 minutes',
+});
+app.use(limiter);
 
 app.use('/api/v1', router);
 
